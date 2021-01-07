adfabdcc9d14cd80c994bf6c8db411c6be145c9d
khamiruf <khairul.amiruf@gmail.com>
2021-01-07 19:29:49 +0800

    add requirements

--- search_history.py
+++ search_history.py
@@ -3,34 +3,37 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
 
-# function to plot word cloud
-def plot_cloud(wordcloud):
-    plt.figure(figsize=(40,30))
-    plt.imshow(wordcloud)
-    plt.axis("off")
+class BHistory():
+    def __init__(self):
+        self.bh = bh.get_browserhistory()
+        self.browsers = self.bh.keys() # shows type of browsers
+            
+    def get_history(self):
+        # chrome -- others cant test atm
+        searches = self.bh['chrome']
+
+        search_history = []
+        # process the searches
+        for search in searches:
+            if 'google' and 'search' in search[0]:
+                if "Google Search" in search[1]:
+                    new_search = search[1].rsplit('-', 1)
+                    print(new_search)
+                    search_history.append(new_search[0])
+        searches = ''.join(search_history)
+        return searches
+
+def plot_wordcloud(searches):
+    # create wc obj
+    wordcloud = WordCloud(width = 3000, height = 2000, random_state=1, background_color='salmon', colormap='Pastel1', collocations=False, stopwords = STOPWORDS).generate(searches)
+    # plot
+    plt.imshow(wordcloud, interpolation='bilInear')
+    plt.axis('off')
+    plt.show()
+
+if __name__ == "__main__":
+    BHist = BHistory()
+    searches= BHist.get_history()
+    plot_wordcloud(searches)
 
 
-hist = bh.get_browserhistory()
-browsers = hist.keys()
-# print(hist['chrome'])
-
-# chrome
-searches = hist['chrome']
-
-search_history = []
-# process the searches
-for search in searches:
-    if 'google' and 'search' in search[0]:
-        print(search[1])
-        search_history.append(search[1])
-
-# print(searches)
-
-# Generate word cloud
-text = ''.join(search_history).lower()
-wordcloud = WordCloud(width = 3000, height = 2000, random_state=1, background_color='salmon', colormap='Pastel1', collocations=False, stopwords = STOPWORDS).generate(text)
-# Plot
-
-plt.imshow(wordcloud, interpolation='bilInear')
-plt.axis('off')
-plt.show()
